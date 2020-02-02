98bcc53406808595b6899693ef0bdc0fa58aeb9c
Kiran Vuksanaj <kiran.vuksanaj@gmail.com>
2020-02-02 17:52:10 -0500

    higher level structure for gradient

--- picmaker.c
+++ picmaker.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 
 #define IMG_FILENAME "image.ppm"
 
+void write_asciiheader(int fd);
+void write_pixel(int fd,int r,int g,int b);
+
 int main(){
-  int fd,r,i;
+  int fd;
+  double r,g,b;
+  int y,x;
   fd = open(IMG_FILENAME,O_CREAT|O_TRUNC|O_WRONLY,0644);
   write_asciiheader(fd);
-  
+  r = 0;
+  for( y=0; y < 500; y++ ){
+    b = y/500.0 * 255;
+    for( x=0; x < 500; x++ ){
+      g = x/500.0 * 255;
+      write_pixel(fd,(int)r,(int)g,(int)b);
+    }
+    write(fd,"\n",1);
+  }
+  close(fd);
 }
